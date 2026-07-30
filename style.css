884397452f71a7d5fe7d1db939911c95121e0d14
yildiz10aa-png <yildiz10aa@gmail.com>
2026-07-30 12:00:33 +0500

    Create style.css

---
+++ style.css
@@ -0,0 +1,81 @@
+*{
+margin:0;
+padding:0;
+box-sizing:border-box;
+font-family:Arial,sans-serif;
+}
+
+body{
+background:#090909;
+display:flex;
+justify-content:center;
+align-items:center;
+min-height:100vh;
+}
+
+.container{
+width:360px;
+background:#111111;
+border:2px solid #FFD700;
+border-radius:20px;
+padding:20px;
+box-shadow:0 0 25px #FFD700;
+}
+
+h1{
+color:#FFD700;
+text-align:center;
+font-size:32px;
+}
+
+h3{
+color:white;
+text-align:center;
+margin-bottom:20px;
+}
+
+input{
+width:100%;
+padding:14px;
+margin:10px 0;
+background:#1c1c1c;
+color:#FFD700;
+border:1px solid #FFD700;
+border-radius:10px;
+font-size:18px;
+}
+
+button{
+width:100%;
+padding:15px;
+background:#FFD700;
+color:black;
+font-weight:bold;
+border:none;
+border-radius:10px;
+font-size:18px;
+cursor:pointer;
+}
+
+button:hover{
+background:#ffea70;
+}
+
+.card{
+margin-top:20px;
+background:#1a1a1a;
+border:1px solid #FFD700;
+border-radius:12px;
+padding:15px;
+}
+
+.card p{
+color:white;
+font-size:18px;
+margin:12px 0;
+}
+
+span{
+color:#FFD700;
+font-weight:bold;
+}
